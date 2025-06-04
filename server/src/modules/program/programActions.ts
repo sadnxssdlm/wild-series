57dae123a014c816594ca7a08fb06d55738a2dea
Sadness <sadnessnc@gmail.com>
2025-06-04 10:46:07 +0200

    feat(step4): display categories and single category pages

--- server/src/modules/program/programActions.ts
+++ server/src/modules/program/programActions.ts
@@ -1,4 +1,4 @@
-import type { RequestHandler } from "express";
+// Some data to make the trick
 
 const programs = [
   {
@@ -23,8 +23,34 @@ const programs = [
   },
 ];
 
+// Declare the actions
+
+import type { RequestHandler } from "express";
+
 const browse: RequestHandler = (req, res) => {
-  res.json(programs);
+  if (req.query.q != null) {
+    const filteredPrograms = programs.filter((program) =>
+      program.synopsis.includes(req.query.q as string),
+    );
+
+    res.json(filteredPrograms);
+  } else {
+    res.json(programs);
+  }
 };
 
-export default { browse };
+const read: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+
+  const program = programs.find((p) => p.id === parsedId);
+
+  if (program != null) {
+    res.json(program);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export them to import them somewhere else
+
+export default { browse, read };
